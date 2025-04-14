40f4d5fde63f731b8a482d10c02f5f14bc61d335
songcw8 <ben990158@gmail.com>
2025-04-14 10:22:57 +0900

    Initial commit

---
+++ src/main/java/org/example/bootblog/BootBlogApplication.java
@@ -0,0 +1,13 @@
+package org.example.bootblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bootblog/BootBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
